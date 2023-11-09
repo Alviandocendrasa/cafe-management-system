a4d8c5e8c24386adc7617922075247a6c0d5ca3b
Alviandocendrasa <alviandocendrasa@gmail.com>
2023-11-10 01:59:36 +0800

    bid schema hook when bidStatus changed to approved

--- server/models/bid.js
+++ server/models/bid.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Workslot = require("./workslot");
 
 const bidSchema = new mongoose.Schema({
   cafeStaffId: {
@@ -22,6 +23,47 @@ const bidSchema = new mongoose.Schema({
     required: true
   }
 });
+
+bidSchema.pre('findOneAndUpdate', async function (next) {
+  try {
+    const query = this.getQuery();
+    const update = this.getUpdate();
+
+    if (update && update.bidStatus === "approved") {
+      // Move jobTitle from pendingJob to approvedJob
+      const dataPrevious = await this.model.findOne(query);
+      const { jobTitle, workslotId } = await this.model.findOne(query);
+      const { pendingJob, approvedJob } = await Workslot.findById(workslotId);
+
+      let updatedPendingJob = [...pendingJob];
+      let updatedApprovedJob = [...approvedJob];
+
+      const index = updatedPendingJob.indexOf(jobTitle);
+
+      if (index > -1) {
+        updatedPendingJob.splice(index, 1);
+        updatedApprovedJob.push(jobTitle)
+
+        await Workslot.findByIdAndUpdate(workslotId, {
+          pendingJob: updatedPendingJob,
+          approvedJob: updatedApprovedJob
+        }, {
+          new: true,
+          runValidators: true
+        })
+
+      } else {
+        console.log("element not found")
+      }
+    }
+
+    next();
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
 
 const Bid = mongoose.model("Bid", bidSchema);
 module.exports = Bid;
