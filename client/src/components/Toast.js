f95c250ce0eed49f957255be2bb741a91abf0c9c
IT <icode67658@gmail.com>
2023-11-15 11:31:50 +0800

    fixed toast bugs

--- client/src/components/Toast.js
+++ client/src/components/Toast.js
@@ -4,15 +4,14 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Toast = ({onSuccessDone, onErrorDone}) => {
-    toast.onChange(payload => {
-        if(onSuccessDone && payload.type === toast.TYPE.SUCCESS && payload.status === "removed") {
-            onSuccessDone();
-        }
-        else if(onErrorDone && payload.type === toast.TYPE.ERROR && payload.status === "removed") {
-            onErrorDone();
-        }
 
-    });
+    const handleToastClose = (event, type, status) => {
+        if (type === toast.TYPE.SUCCESS && status === "removed") {
+            onSuccessDone && onSuccessDone();
+        } else if (type === toast.TYPE.ERROR && status === "removed") {
+            onErrorDone && onErrorDone();
+        }
+    };
     
     return (
           <ToastContainer
@@ -21,6 +20,7 @@ const Toast = ({onSuccessDone, onErrorDone}) => {
             style={{width: "max-content"}}
             limit={3}
             closeOnClick={false}
+            onClose={(event, type, status) => handleToastClose(event, type, status)}
           />
     )
 }
